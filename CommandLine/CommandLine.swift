c15c9f04e5266ab035f622e912f29e7211ed8775
Rocco Bowling <rocco@chimerasw.com>
2018-08-16 16:02:08 -0400

    running the tool with no options now defaults to recording from iphone

--- CommandLine/CommandLine.swift
+++ CommandLine/CommandLine.swift
@@ -42,191 +42,198 @@ let ArgumentAttacher: Character = "="
  * false. You can then call printUsage() to output an automatically-generated usage message.
  */
 open class CommandLine {
-  fileprivate var _arguments: [String]
-  fileprivate var _options: [Option] = [Option]()
-  
-  /**
-   * Initializes a CommandLine object.
-   *
-   * - parameter arguments: Arguments to parse. If omitted, the arguments passed to the app
-   *   on the command line will automatically be used.
-   *
-   * - returns: An initalized CommandLine object.
-   */
-  public init(arguments: [String] = Swift.CommandLine.arguments) {
-    self._arguments = arguments
+    fileprivate var _arguments: [String]
+    fileprivate var _options: [Option] = [Option]()
     
-    /* Initialize locale settings from the environment */
-    setlocale(LC_ALL, "")
-  }
-  
-  /* Returns all argument values from flagIndex to the next flag or the end of the argument array. */
-  fileprivate func _getFlagValues(flagIndex: Int) -> [String] {
-    var args: [String] = [String]()
-    var skipFlagChecks = false
-    
-    /* Grab attached arg, if any */
-    var attachedArg = _arguments[flagIndex].splitByCharacter(ArgumentAttacher, maxSplits: 1)
-    if attachedArg.count > 1 {
-      args.append(attachedArg[1])
+    /**
+     * Initializes a CommandLine object.
+     *
+     * - parameter arguments: Arguments to parse. If omitted, the arguments passed to the app
+     *   on the command line will automatically be used.
+     *
+     * - returns: An initalized CommandLine object.
+     */
+    public init(arguments: [String] = Swift.CommandLine.arguments) {
+        self._arguments = arguments
+        
+        /* Initialize locale settings from the environment */
+        setlocale(LC_ALL, "")
     }
     
-    for i in flagIndex + 1 ..< _arguments.count {
-      if !skipFlagChecks {
-        if _arguments[i] == ArgumentStopper {
-          skipFlagChecks = true
-          continue
+    /* Returns all argument values from flagIndex to the next flag or the end of the argument array. */
+    fileprivate func _getFlagValues(flagIndex: Int) -> [String] {
+        var args: [String] = [String]()
+        var skipFlagChecks = false
+        
+        /* Grab attached arg, if any */
+        var attachedArg = _arguments[flagIndex].splitByCharacter(ArgumentAttacher, maxSplits: 1)
+        if attachedArg.count > 1 {
+            args.append(attachedArg[1])
         }
         
-        if _arguments[i].hasPrefix(ShortOptionPrefix) && Int(_arguments[i]) == nil &&
-          _arguments[i].toDouble() == nil {
-          break
-        }
-      }
-    
-      args.append(_arguments[i])
-    }
-    
-    return args
-  }
-  
-  /**
-   * Adds an Option to the command line.
-   *
-   * - parameter option: The option to add.
-   */
-  open func addOption(_ option: Option) {
-    _options.append(option)
-  }
-  
-  /**
-   * Adds one or more Options to the command line.
-   *
-   * - parameter options: An array containing the options to add.
-   */
-  open func addOptions(_ options: [Option]) {
-    _options += options
-  }
-  
-  /**
-   * Adds one or more Options to the command line.
-   *
-   * - parameter options: The options to add.
-   */
-  open func addOptions(_ options: Option...) {
-    _options += options
-  }
-  
-  /**
-   * Sets the command line Options. Any existing options will be overwritten.
-   *
-   * - parameter options: An array containing the options to set.
-   */
-  open func setOptions(_ options: [Option]) {
-    _options = options
-  }
-  
-  /**
-   * Sets the command line Options. Any existing options will be overwritten.
-   *
-   * - parameter options: The options to set.
-   */
-  open func setOptions(_ options: Option...) {
-    _options = options
-  }
-  
-  /**
-   * Parses command-line arguments into their matching Option values.
-   *
-   * - returns: True if all arguments were parsed successfully, false if any option had an
-   *   invalid value or if a required option was missing.
-   */
-  open func parse() -> (Bool, String?) {
-    
-    for (idx, arg) in _arguments.enumerated() {
-      if arg == ArgumentStopper {
-        break
-      }
-      
-      if !arg.hasPrefix(ShortOptionPrefix) {
-        continue
-      }
-      
-      /* Swift strings don't have substringFromIndex(). Do a little dance instead. */
-      var flag = ""
-      var skipChars =
-        arg.hasPrefix(LongOptionPrefix) ? LongOptionPrefix.characters.count : ShortOptionPrefix.characters.count
-      for c in arg.characters {
-        if skipChars > 0 {
-          skipChars -= 1
-          continue
-        }
-        
-        flag.append(c)
-      }
-      
-      /* Remove attached argument from flag */
-      flag = flag.splitByCharacter(ArgumentAttacher, maxSplits: 1)[0]
-      
-      var flagMatched = false
-      for option in _options {
-        if flag == option.shortFlag || flag == option.longFlag {
-          let vals = self._getFlagValues(flagIndex: idx)
-          if !option.match(vals) {
-            return (false, "Invalid value for \(option.longFlag)")
-          }
-          
-          flagMatched = true
-          break
-        }
-      }
-      
-      /* Flags that do not take any arguments can be concatenated */
-      if !flagMatched && !arg.hasPrefix(LongOptionPrefix) {
-        for (i, c) in flag.characters.enumerated() {
-          let flagLength = flag.characters.count
-          for option in _options {
-            if String(c) == option.shortFlag {
-              /* Values are allowed at the end of the concatenated flags, e.g.
-               * -xvf <file1> <file2>
-               */
-              let vals = (i == flagLength - 1) ? self._getFlagValues(flagIndex: idx) : [String]()
-              if !option.match(vals) {
-                return (false, "Invalid value for \(option.longFlag)")
-              }
-              
-              break
+        for i in flagIndex + 1 ..< _arguments.count {
+            if !skipFlagChecks {
+                if _arguments[i] == ArgumentStopper {
+                    skipFlagChecks = true
+                    continue
+                }
+                
+                if _arguments[i].hasPrefix(ShortOptionPrefix) && Int(_arguments[i]) == nil &&
+                    _arguments[i].toDouble() == nil {
+                    break
+                }
             }
-          }
+            
+            args.append(_arguments[i])
         }
-      }
-    }
-
-    /* Check to see if any required options were not matched */
-    for option in _options {
-      if option.required && !option.isSet {
-        return (false, "\(option.longFlag) is required")
-      }
+        
+        return args
     }
     
-    return (true, nil)
-  }
-  
-  /** Prints a usage message to stdout. */
-  open func printUsage() {
-    let name = _arguments[0]
-    
-    var flagWidth = 0
-    for opt in _options {
-      flagWidth = max(flagWidth,
-        "  \(ShortOptionPrefix)\(opt.shortFlag), \(LongOptionPrefix)\(opt.longFlag):".characters.count)
+    /**
+     * Adds an Option to the command line.
+     *
+     * - parameter option: The option to add.
+     */
+    open func addOption(_ option: Option) {
+        _options.append(option)
     }
     
-    print("Usage: \(name) [options]")
-    for opt in _options {
-      let flags = "  \(ShortOptionPrefix)\(opt.shortFlag), \(LongOptionPrefix)\(opt.longFlag):".paddedToWidth(flagWidth)
-      
-      print("\(flags)\n      \(opt.helpMessage)")
+    /**
+     * Adds one or more Options to the command line.
+     *
+     * - parameter options: An array containing the options to add.
+     */
+    open func addOptions(_ options: [Option]) {
+        _options += options
     }
-  }
+    
+    /**
+     * Adds one or more Options to the command line.
+     *
+     * - parameter options: The options to add.
+     */
+    open func addOptions(_ options: Option...) {
+        _options += options
+    }
+    
+    /**
+     * Sets the command line Options. Any existing options will be overwritten.
+     *
+     * - parameter options: An array containing the options to set.
+     */
+    open func setOptions(_ options: [Option]) {
+        _options = options
+    }
+    
+    /**
+     * Sets the command line Options. Any existing options will be overwritten.
+     *
+     * - parameter options: The options to set.
+     */
+    open func setOptions(_ options: Option...) {
+        _options = options
+    }
+    
+    /**
+     * Parses command-line arguments into their matching Option values.
+     *
+     * - returns: True if all arguments were parsed successfully, false if any option had an
+     *   invalid value or if a required option was missing.
+     */
+    open func parse() -> (Bool, String?) {
+        
+        /* set the default values if they exist */
+        for option in _options {
+            if option.defaultValue != nil {
+                option.match([option.defaultValue!])
+            }
+        }
+        
+        for (idx, arg) in _arguments.enumerated() {
+            if arg == ArgumentStopper {
+                break
+            }
+            
+            if !arg.hasPrefix(ShortOptionPrefix) {
+                continue
+            }
+            
+            /* Swift strings don't have substringFromIndex(). Do a little dance instead. */
+            var flag = ""
+            var skipChars =
+                arg.hasPrefix(LongOptionPrefix) ? LongOptionPrefix.characters.count : ShortOptionPrefix.characters.count
+            for c in arg.characters {
+                if skipChars > 0 {
+                    skipChars -= 1
+                    continue
+                }
+                
+                flag.append(c)
+            }
+            
+            /* Remove attached argument from flag */
+            flag = flag.splitByCharacter(ArgumentAttacher, maxSplits: 1)[0]
+            
+            var flagMatched = false
+            for option in _options {
+                if flag == option.shortFlag || flag == option.longFlag {
+                    let vals = self._getFlagValues(flagIndex: idx)
+                    if !option.match(vals) {
+                        return (false, "Invalid value for \(option.longFlag)")
+                    }
+                    
+                    flagMatched = true
+                    break
+                }
+            }
+            
+            /* Flags that do not take any arguments can be concatenated */
+            if !flagMatched && !arg.hasPrefix(LongOptionPrefix) {
+                for (i, c) in flag.characters.enumerated() {
+                    let flagLength = flag.characters.count
+                    for option in _options {
+                        if String(c) == option.shortFlag {
+                            /* Values are allowed at the end of the concatenated flags, e.g.
+                             * -xvf <file1> <file2>
+                             */
+                            let vals = (i == flagLength - 1) ? self._getFlagValues(flagIndex: idx) : [String]()
+                            if !option.match(vals) {
+                                return (false, "Invalid value for \(option.longFlag)")
+                            }
+                            
+                            break
+                        }
+                    }
+                }
+            }
+        }
+        
+        /* Check to see if any required options were not matched */
+        for option in _options {
+            if option.required && !option.isSet {
+                return (false, "\(option.longFlag) is required")
+            }
+        }
+        
+        return (true, nil)
+    }
+    
+    /** Prints a usage message to stdout. */
+    open func printUsage() {
+        let name = _arguments[0]
+        
+        var flagWidth = 0
+        for opt in _options {
+            flagWidth = max(flagWidth,
+                            "  \(ShortOptionPrefix)\(opt.shortFlag), \(LongOptionPrefix)\(opt.longFlag):".characters.count)
+        }
+        
+        print("Usage: \(name) [options]")
+        for opt in _options {
+            let flags = "  \(ShortOptionPrefix)\(opt.shortFlag), \(LongOptionPrefix)\(opt.longFlag):".paddedToWidth(flagWidth)
+            
+            print("\(flags)\n      \(opt.helpMessage)")
+        }
+    }
 }
--- CommandLine/Option.swift
+++ CommandLine/Option.swift
@@ -19,36 +19,38 @@
  * The base class for a command-line option.
  */
 open class Option {
-  var shortFlag: String
-  var longFlag: String
-  var required: Bool
-  var helpMessage: String
-  
-  /* Override this property to test _value for nil on each Option subclass.
-   *
-   * This is necessary to support nil checks on an array of Options (see
-   * CommandLine.parse()) because Swift doesn't allow overriding property
-   * declarations with differing types, and methods (unlike functions) are not
-   * covariant as of beta 4.
-   */
-  var isSet: Bool {
-    return false
-  }
-  
-  public init(shortFlag: String, longFlag: String, required: Bool, helpMessage: String) {
-    assert(shortFlag.characters.count == 1, "Short flag must be a single character")
-    assert(Int(shortFlag) == nil && shortFlag.toDouble() == nil, "Short flag cannot be a numeric value")
-    assert(Int(longFlag) == nil && longFlag.toDouble() == nil, "Long flag cannot be a numeric value")
+    var shortFlag: String
+    var longFlag: String
+    var required: Bool
+    var helpMessage: String
+    var defaultValue: String?
     
-    self.shortFlag = shortFlag
-    self.longFlag = longFlag
-    self.helpMessage = helpMessage
-    self.required = required
-  }
-  
-  func match(_ values: [String]) -> Bool {
-    return false
-  }
+    /* Override this property to test _value for nil on each Option subclass.
+     *
+     * This is necessary to support nil checks on an array of Options (see
+     * CommandLine.parse()) because Swift doesn't allow overriding property
+     * declarations with differing types, and methods (unlike functions) are not
+     * covariant as of beta 4.
+     */
+    var isSet: Bool {
+        return false
+    }
+    
+    public init(shortFlag: String, longFlag: String, required: Bool, defaultValue:String?, helpMessage: String) {
+        assert(shortFlag.characters.count == 1, "Short flag must be a single character")
+        assert(Int(shortFlag) == nil && shortFlag.toDouble() == nil, "Short flag cannot be a numeric value")
+        assert(Int(longFlag) == nil && longFlag.toDouble() == nil, "Long flag cannot be a numeric value")
+        
+        self.shortFlag = shortFlag
+        self.longFlag = longFlag
+        self.helpMessage = helpMessage
+        self.required = required
+        self.defaultValue = defaultValue
+    }
+    
+    func match(_ values: [String]) -> Bool {
+        return false
+    }
 }
 
 /**
@@ -56,51 +58,51 @@ open class Option {
  * absence of the flag(s) is equivalent to false.
  */
 open class BoolOption: Option {
-  fileprivate var _value: Bool = false
-  
-  open var value: Bool {
-    return _value
-  }
-  
-  override var isSet: Bool {
-    /* BoolOption is always set; if missing from the command line, it's false */
-    return true
-  }
-  
-  public init(shortFlag: String, longFlag: String, helpMessage: String) {
-    super.init(shortFlag: shortFlag, longFlag: longFlag, required: false, helpMessage: helpMessage)
-  }
-  
-  override func match(_ values: [String]) -> Bool {
-    _value = true
-    return true
-  }
+    fileprivate var _value: Bool = false
+    
+    open var value: Bool {
+        return _value
+    }
+    
+    override var isSet: Bool {
+        /* BoolOption is always set; if missing from the command line, it's false */
+        return true
+    }
+    
+    public init(shortFlag: String, longFlag: String, defaultValue: String?, helpMessage: String) {
+        super.init(shortFlag: shortFlag, longFlag: longFlag, required: false, defaultValue: defaultValue, helpMessage: helpMessage)
+    }
+    
+    override func match(_ values: [String]) -> Bool {
+        _value = true
+        return true
+    }
 }
 
 /**  An option that accepts a positive or negative integer value. */
 open class IntOption: Option {
-  fileprivate var _value: Int?
-  
-  open var value: Int? {
-    return _value
-  }
-  
-  override var isSet: Bool {
-    return _value != nil
-  }
-  
-  override func match(_ values: [String]) -> Bool {
-    if values.count == 0 {
-      return false
+    fileprivate var _value: Int?
+    
+    open var value: Int? {
+        return _value
     }
     
-    if let val = Int(values[0]) {
-      _value = val
-      return true
+    override var isSet: Bool {
+        return _value != nil
     }
     
-    return false
-  }
+    override func match(_ values: [String]) -> Bool {
+        if values.count == 0 {
+            return false
+        }
+        
+        if let val = Int(values[0]) {
+            _value = val
+            return true
+        }
+        
+        return false
+    }
 }
 
 /**
@@ -108,122 +110,122 @@ open class IntOption: Option {
  * on the command-line, the counter will be incremented.
  */
 open class CounterOption: Option {
-  fileprivate var _value: Int = 0
-  
-  open var value: Int {
-    return _value
-  }
-  
-  override var isSet: Bool {
-    /* CounterOption is always set; if missing from the command line, it's 0 */
-    return true
-  }
-  
-  public init(shortFlag: String, longFlag: String, helpMessage: String) {
-    super.init(shortFlag: shortFlag, longFlag: longFlag, required: false, helpMessage: helpMessage)
-  }
-  
-  override func match(_ values: [String]) -> Bool {
-    _value += 1
-    return true
-  }
+    fileprivate var _value: Int = 0
+    
+    open var value: Int {
+        return _value
+    }
+    
+    override var isSet: Bool {
+        /* CounterOption is always set; if missing from the command line, it's 0 */
+        return true
+    }
+    
+    public init(shortFlag: String, longFlag: String, defaultValue: String?, helpMessage: String) {
+        super.init(shortFlag: shortFlag, longFlag: longFlag, required: false, defaultValue: defaultValue, helpMessage: helpMessage)
+    }
+    
+    override func match(_ values: [String]) -> Bool {
+        _value += 1
+        return true
+    }
 }
 
 /**  An option that accepts a positive or negative floating-point value. */
 open class DoubleOption: Option {
-  fileprivate var _value: Double?
-  
-  open var value: Double? {
-    return _value
-  }
-  
-  override var isSet: Bool {
-    return _value != nil
-  }
-  
-  override func match(_ values: [String]) -> Bool {
-    if values.count == 0 {
-      return false
+    fileprivate var _value: Double?
+    
+    open var value: Double? {
+        return _value
     }
     
-    if let val = values[0].toDouble() {
-      _value = val
-      return true
+    override var isSet: Bool {
+        return _value != nil
     }
     
-    return false
-  }
+    override func match(_ values: [String]) -> Bool {
+        if values.count == 0 {
+            return false
+        }
+        
+        if let val = values[0].toDouble() {
+            _value = val
+            return true
+        }
+        
+        return false
+    }
 }
 
 /**  An option that accepts a string value. */
 open class StringOption: Option {
-  fileprivate var _value: String? = nil
-  
-  open var value: String? {
-    return _value
-  }
-  
-  override var isSet: Bool {
-    return _value != nil
-  }
-  
-  override func match(_ values: [String]) -> Bool {
-    if values.count == 0 {
-      return false
+    fileprivate var _value: String? = nil
+    
+    open var value: String? {
+        return _value
     }
     
-    _value = values[0]
-    return true
-  }
+    override var isSet: Bool {
+        return _value != nil
+    }
+    
+    override func match(_ values: [String]) -> Bool {
+        if values.count == 0 {
+            return false
+        }
+        
+        _value = values[0]
+        return true
+    }
 }
 
 /**  An option that accepts one or more string values. */
 open class MultiStringOption: Option {
-  fileprivate var _value: [String]?
-  
-  open var value: [String]? {
-    return _value
-  }
-  
-  override var isSet: Bool {
-    return _value != nil
-  }
-  
-  override func match(_ values: [String]) -> Bool {
-    if values.count == 0 {
-      return false
+    fileprivate var _value: [String]?
+    
+    open var value: [String]? {
+        return _value
     }
     
-    _value = values
-    return true
-  }
+    override var isSet: Bool {
+        return _value != nil
+    }
+    
+    override func match(_ values: [String]) -> Bool {
+        if values.count == 0 {
+            return false
+        }
+        
+        _value = values
+        return true
+    }
 }
 
 /** An option that represents an enum value. */
 open class EnumOption<T:RawRepresentable>: Option where T.RawValue == String {
-  fileprivate var _value: T?
-  open var value: T? {
-    return _value
-  }
-  
-  override var isSet: Bool {
-    return _value != nil
-  }
-  
-  override public init(shortFlag: String, longFlag: String, required: Bool, helpMessage: String) {
-    super.init(shortFlag: shortFlag, longFlag: longFlag, required: required, helpMessage: helpMessage)
-  }
-  
-  override func match(_ values: [String]) -> Bool {
-    if values.count == 0 {
-      return false
+    fileprivate var _value: T?
+    open var value: T? {
+        return _value
     }
     
-    if let v = T(rawValue: values[0]) {
-      _value = v
-      return true
+    override var isSet: Bool {
+        return _value != nil
     }
     
-    return false
-  }
+    override public init(shortFlag: String, longFlag: String, required: Bool, defaultValue: String?, helpMessage: String) {
+        super.init(shortFlag: shortFlag, longFlag: longFlag, required: required, defaultValue: defaultValue, helpMessage: helpMessage)
+    }
+    
+    override func match(_ values: [String]) -> Bool {
+        if values.count == 0 {
+            return false
+        }
+        
+        if let v = T(rawValue: values[0]) {
+            _value = v
+            return true
+        }
+        
+        return false
+    }
 }
